002acda2112be169c62f15d1c06b2f3e744ee377
Michael Fulton <fulto081@umn.edu>
2022-12-01 23:31:04 -0600

    Update communication_policy.py

--- scripts/acvs/communication_policy.py
+++ scripts/acvs/communication_policy.py
@@ -1,25 +1,29 @@
 class CommunicationPolicy(object):
-    def __init__(self):
-        pass
+    def __init__(self, language):
+        self.language = language
 
+
+    def test():
+        raise NotImplementedError()
 
 
 class StaticPolicy(CommunicationPolicy):
-    def __init__(self):
-        pass
+    def __init__(self, language, vector):
+        super().init(self, language)
+        self.static_vector = vector
 
 class RandomPolicy(CommunicationPolicy):
-    def __init__(self):
-        pass
+    def __init__(self, language):
+        super().init(self, language)
 
 class HeuristicPolicy(CommunicationPolicy):
-    def __init__(self):
-        pass
+    def __init__(self, language):
+        super().init(self, language)
 
 class POMDPPolicy(CommunicationPolicy):
-    def __init__(self):
-        pass
+    def __init__(self, language):
+        super().init(self, language)
 
 class PersonalPolicy(CommunicationPolicy):
-    def __init__(self):
-        pass
+    def __init__(self, language):
+        super().init(self, language)
